de7aa193d80c9740bbbd45e358d9805ff59145e9
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2019-11-29 11:53:11 +0100

    Using lazy inside SDK v2

--- Tests/Cosmos/Repository.cs
+++ Tests/Cosmos/Repository.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Net;
 using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
 
 namespace Tests.Cosmos
@@ -7,18 +10,41 @@ namespace Tests.Cosmos
     public class Repository
     {
         private readonly Settings _settings;
-        private readonly DocumentClient _client;
+        private readonly Lazy<DocumentClient> _client;
 
         public Repository(Settings settings)
         {
             _settings = settings;
 
-            _client = new DocumentClient(new Uri(_settings.Endpoint), _settings.Key);
+            _client = new Lazy<DocumentClient>(() =>
+            {
+                var client = new DocumentClient(new Uri(_settings.Endpoint), _settings.Key);
+                PrepareDatabase(client).ConfigureAwait(false).GetAwaiter().GetResult();
+                return client;
+            });
+        }
+
+        private async Task<string> PrepareDatabase(DocumentClient client)
+        {
+            try
+            {
+                var response = await client.ReadDatabaseAsync(UriFactory.CreateDatabaseUri(_settings.DatabaseId)).ConfigureAwait(false);
+                return _settings.DatabaseId;
+            }
+            catch (DocumentClientException de)
+            {
+                if (de.StatusCode == HttpStatusCode.NotFound)
+                {
+                    Trace.WriteLine($"Database {_settings.DatabaseId} doesn't exist!");
+                }
+
+                throw;
+            }
         }
 
         public async Task ReadDatabase()
         {
-            await _client.ReadDatabaseAsync(UriFactory.CreateDatabaseUri(_settings.DatabaseId));
+            await _client.Value.ReadDatabaseAsync(UriFactory.CreateDatabaseUri(_settings.DatabaseId));
         }
     }
 }
