c462cd202f759fea0c32d64831d7f3c6b47681be
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2019-11-29 11:41:43 +0100

    SDK v2

--- Tests/Cosmos/Repository.cs
+++ Tests/Cosmos/Repository.cs
@@ -1,44 +1,24 @@
 ﻿using System;
-using System.Net;
 using System.Threading.Tasks;
-using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Documents.Client;
 
 namespace Tests.Cosmos
 {
     public class Repository
     {
         private readonly Settings _settings;
-        private readonly Lazy<Database> _clientFactory;
-        private readonly CosmosClient _cosmosClient;
+        private readonly DocumentClient _client;
 
         public Repository(Settings settings)
         {
-            _settings = settings ?? throw new ArgumentNullException(nameof(settings));
+            _settings = settings;
 
-            _cosmosClient = new CosmosClient(settings.Endpoint, settings.Key);
-            _clientFactory = new Lazy<Database>(() =>
-            {
-                var database = CheckIfDatabaseExists(settings).ConfigureAwait(false).GetAwaiter().GetResult();
-
-                return database;
-            });
-        }
-
-        private async Task<Database> CheckIfDatabaseExists(Settings settings)
-        {
-            var database = _cosmosClient.GetDatabase(settings.DatabaseId);
-            var read = await database.ReadStreamAsync();
-            if (read.StatusCode == HttpStatusCode.NotFound)
-            {
-                throw new Exception($"CosmosDB database {settings.DatabaseId} does not exist!");
-            }
-
-            return database;
+            _client = new DocumentClient(new Uri(_settings.Endpoint), _settings.Key);
         }
 
         public async Task ReadDatabase()
         {
-            await _clientFactory.Value.ReadAsync();
+            await _client.ReadDatabaseAsync(UriFactory.CreateDatabaseUri(_settings.DatabaseId));
         }
     }
 }
